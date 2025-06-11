d08df4e759f56f7635430dd5e9c5394cba091708
MoisesPiguave <moi.emanuelps@gmail.com>
2025-06-11 18:27:36 -0500

    Trabajo hecho, creo

--- src/App.java
+++ src/App.java
@@ -1,18 +1,26 @@
+import Controller.ControllerDao;
+import Controller.MenuController;
 import Model.Contact;
+import View.ConsoleView;
 
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Contact c1 = new Contact("Juan", "123");
-        Contact c4 = new Contact("Juan");
-        Contact c2 = new Contact("Juan", null);
-        Contact c3 = new Contact("Juana", "456");
-        Contact c5 = new Contact("Juan", "123");
-        System.out.println(c1.equals(c2));
-        System.out.println(c1.equals(c3));
-        System.out.println(c1.equals(c4));
-        System.out.println(c1.equals(c5));
+        //Contact c1 = new Contact("Juan", "123");
+      //Contact c4 = new Contact("Juan");
+        //Contact c2 = new Contact("Juan", null);
+        //Contact c3 = new Contact("Juana", "456");
+        //Contact c5 = new Contact("Juan", "123");
+        //System.out.println(c1.equals(c2));
+        //System.out.println(c1.equals(c3));
+        //System.out.println(c1.equals(c4));
+        //System.out.println(c1.equals(c5));
+
+        ControllerDao contactManager = new ControllerDao();
+        ConsoleView view = new ConsoleView();
+        MenuController controller = new MenuController(contactManager, view);
+        controller.showMenu();
 
     }
 }
--- src/Controller/MenuController.java
+++ src/Controller/MenuController.java
@@ -1,2 +1,76 @@
 package Controller;
 
+import View.ConsoleView;
+import Model.Contact;
+
+public class MenuController {
+    private ControllerDao contactManager;
+    private ConsoleView consoleView;
+
+    public MenuController(ControllerDao contactManager, ConsoleView consoleView) {
+        this.contactManager = contactManager;
+        this.consoleView = consoleView;
+    }
+
+    public void showMenu() {
+        boolean running = true;
+        while (running) {
+            consoleView.displayMenu();
+            String choice = consoleView.getInput("Seleccione una opción: ");
+
+            switch (choice) {
+                case "1":
+                    addContact();
+                    break;
+                case "2":
+                    findContact();
+                    break;
+                case "3":
+                    deleteContact();
+                    break;
+                case "4":
+                    printContacts();
+                    break;
+                case "5":
+                    running = false;
+                    consoleView.showMessage("Saliendo");
+                    break;
+                default:
+                    consoleView.showMessage(" Intente otra vez ");
+            }
+        }
+    }
+
+    public void addContact() {
+        String name = consoleView.getInput("Ingrese el nombre: ");
+        String phone = consoleView.getInput("Ingrese el teléfono: ");
+        Contact contact = new Contact(name, phone);
+        contactManager.addContact(contact);
+        consoleView.showMessage("Contacto agregado ");
+    }
+
+    public void findContact() {
+        String name = consoleView.getInput("Ingrese el nombre del contacto  ");
+        Contact contact = contactManager.buscarContacto(name);
+        if (contact != null) {
+            consoleView.showMessage("Contacto encontrado: " + contact);
+        } else {
+            consoleView.showMessage("Contacto no encontrado.");
+        }
+    }
+
+    public void deleteContact() {
+        String name = consoleView.getInput("Ingrese el nombre del contacto  ");
+        boolean deleted = contactManager.eliminarContacto(name);
+        if (deleted) {
+            consoleView.showMessage("Contacto eliminado.");
+        } else {
+            consoleView.showMessage("No se encontró el contacto.");
+        }
+    }
+
+    public void printContacts() {
+        contactManager.imprimirContacto();
+    }
+}
+
